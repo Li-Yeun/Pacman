259c7a8ce2c06d893d058a313364fe9f0e20e528
pjeromaster <pjeromaster@gmail.com>
2018-12-02 14:19:04 +0100

    Fix

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,44 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Movement : MonoBehaviour {
-    public Rigidbody rb;
+    public Rigidbody rb;    
     public Transform tf;
-    public Trigger front;
-    public Trigger back;
-    public Trigger left;
-    public Trigger right;
-    public string[] Controls = new string[4];
-    public int speed = 100;
-    float cooldown = 0;
-    Vector3 Facing;
+    public Trigger front, left, right;      //colliders voor navigatie
+    public string[] Controls = new string[2];       //controlls van spookje
+    public int speed = 100;     //beweegsnelheid
+    public int Respawntime = 4;         //tijd voordat speler weer kan besturen
+    float RotationCooldown = 0;         //tijd voordat er weer gedraaid kan worden
+    Vector3 Velocity;         //beweegsnelheid
+    public NavMeshAgent agent;          //navigatie voor teruggaan naar spawn
+    public Transform respawn;           //locatie van spawnpunt
+    public bool dead = false;           //als true is de speler doodgegaan
+    float respawntimer = 0;         
+
     void Start ()
     {
-		
+        agent.enabled = false;
 	}
 	
 	void Update ()
     {
-		
+        if (dead)
+        {
+            agent.enabled = true;
+            respawntimer += Time.deltaTime;
+            if(respawntimer >= Respawntime)
+            {
+                dead = false;
+                agent.enabled = false;
+                tf.eulerAngles = new Vector3(0, 0, 0);
+                respawntimer = 0;
+                
+            }
+            agent.SetDestination(respawn.position);
+
+        }
 	}
 
     void FixedUpdate()
     {
-        Debug.Log((int)tf.eulerAngles.y);
-        MoveForward();
-        HandleInput();
-        if (front.Collision||back.Collision)
+        if (!dead)
         {
-            if (!right.Collision && cooldown == 0)
+            MoveForward();
+            HandleInput();
+            if (front.Collision)
             {
-                Rotate(-1);
+                if (!right.Collision && RotationCooldown == 0)
+                {
+                    Rotate(1);
 
-            }
-            else if (!left.Collision && cooldown == 0)
-            {
-                Rotate(1); 
+                }
+                else if (!left.Collision && RotationCooldown == 0)
+                {
+                    Rotate(-1);
 
+                }
+                else if (left.Collision && right.Collision)
+                {
+                    Rotate(2);
+                }
             }
         }
 
@@ -49,44 +72,44 @@ public class Movement : MonoBehaviour {
         switch ((int)tf.eulerAngles.y)
         {
             case 0:
-                Facing = new Vector3(-Time.deltaTime,0,0);
+                Velocity = new Vector3(Time.deltaTime,0,0);
                 break;
             case 90:
-                Facing = new Vector3(0, 0,Time.deltaTime);
+                Velocity = new Vector3(0, 0,-Time.deltaTime);
                 break;
             case 180:
-                Facing = new Vector3(Time.deltaTime, 0,0);
+                Velocity = new Vector3(-Time.deltaTime, 0,0);
                 break;
             case 270:
-                Facing = new Vector3(0,0,-Time.deltaTime);
+                Velocity = new Vector3(0,0,Time.deltaTime);
                 break;
         }
 
-        tf.position += Facing * speed;    
+        tf.position += Velocity * speed;    
     }
 
     void Rotate(int i)
     {
         tf.Rotate(0,i*90,0);
-        cooldown = 1;
+        RotationCooldown = 1;
     }
 
     void HandleInput()
     {
-        if (Input.GetKey(Controls[0]) && cooldown == 0)
+        if (Input.GetKey(Controls[0]) && RotationCooldown == 0)
         {
             Rotate(1); 
         }
-        if (Input.GetKey(Controls[1]) && cooldown == 0)
+        if (Input.GetKey(Controls[1]) && RotationCooldown == 0)
         {
             Rotate(-1);
         }
-        if(cooldown >= 1)
+        if(RotationCooldown >= 1)
         {
-            cooldown += Time.deltaTime;
-            if(cooldown >= 1.2f)
+            RotationCooldown += Time.deltaTime;
+            if(RotationCooldown >= 1.2f)
             {
-                cooldown = 0;
+                RotationCooldown = 0;
             }
         }
     }
