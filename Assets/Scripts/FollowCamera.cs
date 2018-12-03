dfba98a06c2ccda85a57dfd23530110719ad2716
TinusPeterse <43200125+TinusPeterse@users.noreply.github.com>
2018-12-03 14:12:23 +0100

    Update FollowCamera.cs

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This Class makes the camera follow the target
+/// </summary>
 public class FollowCamera : MonoBehaviour {
 
     public GameObject FollowTarget;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void LateUpdate() {
+	void LateUpdate()
+    {
         transform.position = new Vector3(FollowTarget.transform.position.x, transform.position.y, FollowTarget.transform.position.z);
 	}
 }
