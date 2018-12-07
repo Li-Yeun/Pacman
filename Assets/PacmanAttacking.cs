f23d26e0880846bf6d1c0bd497e2f7ed854d230d
TinusPeterse <43200125+TinusPeterse@users.noreply.github.com>
2018-12-07 13:50:32 +0100

    PacmanHuntAnimation
    Press 8 for effects.

---
+++ Assets/PacmanAttacking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanAttacking : MonoBehaviour {
+
+    Animator animator;
+    float TimePassed = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        animator.Play("SpookAnimationBlue");
+        animator.SetBool("PacmanIsOnTheHunt", false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("8"))
+            animator.SetBool("PacmanIsOnTheHunt", true);
+	}
+}
