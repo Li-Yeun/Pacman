73573a59f58a6611e9bbe9bcd887d261aff90c4a
mike groffen <mikegroffen4@gmail.com>
2018-12-10 10:47:31 +0100

    pausemenu en hud

---
+++ Assets/levelUI/HUD.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HUD : MonoBehaviour {
+
+    public Button continu;
+    public Button mainmenu;
+    public Button options;
+    public Button quitgame;
+    public Button pause;
+    public string gamescene;
+    public GameObject pausemenu;
+    public GameObject optionmenu;
+
+    public void Pause()
+    {
+        pausemenu.SetActive(true);
+        
+    }
+
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene(gamescene);
+    }
+
+    public void Options()
+    {
+        optionmenu.SetActive(true);
+    }
+
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+
+
+    public void Continu()
+    {
+        pausemenu.SetActive(false);
+    }
+}
---
+++ Assets/levelUI/playerhealth.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerhealth : MonoBehaviour {
+
+   
+    int health = 3;
+    public Texture2D Health3;
+    public Texture2D Health2;
+    public Texture2D Health1;
+    public Texture2D currentHealth;
+
+    void Update () {
+        if (health <= 0)
+        { Dead(); }
+        Healthbar();
+	}
+
+    void Dead()
+    { }
+
+    public void Healthbar()
+    {
+        if (health == 3)
+        { currentHealth = Health3; }
+            else if (health == 2)
+            { currentHealth = Health2; }
+                else if (health == 1)
+                    { currentHealth = Health1; }
+    }
+
+    
+
+    
+}
