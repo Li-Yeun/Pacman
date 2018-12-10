dfef086e69453bd1cc78508fbdeeebff17407170
mike groffen <mikegroffen4@gmail.com>
2018-12-10 17:25:19 +0100

    UI

--- Assets/mainmenu/aboutmenu/aboutmenu.cs
+++ Assets/mainmenu/aboutmenu/aboutmenu.cs
@@ -6,9 +6,27 @@ public class aboutmenu : MonoBehaviour {
 
     public Button Back;
     public GameObject About;
+    public int width;
+    public int height;
+    public Button video;
+    public GameObject Video;
+
+    public void Setwidth(int Width)
+    { width = Width; }
+
+    public void Setheight(int Height)
+    { height = Height; }
+
+    public void Setresolution()
+    {
+        Screen.SetResolution(width, height, false);
+    }
 
     public void back()
     {
         About.SetActive(false);
     }
+
+    public void Videomenu()
+    { Video.SetActive(true); }
 }
