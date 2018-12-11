4f32c6f0a14ca23f466ff9ee496d00061e4c36d0
Li-Yeun xu <liyeun@live.nl>
2018-12-11 14:00:30 +0100

    MultiPlayer Test

---
+++ Assets/MultiPlayer/OnlineMovement.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Insert alle functies van wat deze classe doet. Thanks.
+/// </summary>
+
+public class OnlineMovement : NetworkBehaviour {
+    public Rigidbody rb;    
+    public Transform tf;    
+    public Trigger front, left, right;               //colliders voor navigatie
+    public string[] Controls = new string[2];        //controlls van spookje
+    public int speed = 100;                          //beweegsnelheid
+    public int Respawntime = 4;                      //tijd voordat speler weer kan besturen
+    float RotationCooldown = 0;                      //tijd voordat er weer gedraaid kan worden
+    Vector3 Velocity;                                //beweegsnelheid
+    public NavMeshAgent agent;                       //navigatie voor teruggaan naar spawn
+    public Transform respawn;                        //locatie van spawnpunt
+    public bool dead = false;                        //als true is de speler doodgegaan
+    public bool UseRandom;
+    float respawntimer = 0;         
+
+    void Start ()
+    {
+        agent.enabled = false;
+	}
+
+    
+    void Update ()
+    {
+        if (!hasAuthority)
+            return;
+        if (dead)
+        {
+            DoRespawn();
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (!hasAuthority)
+            return;
+
+        if (!dead)
+        {
+            MoveForward();
+            HandleInput();
+            AutoNav();
+        }
+    }
+
+    void MoveForward()
+    {
+        switch ((int)tf.eulerAngles.y)
+        {
+            case 0:
+                Velocity = new Vector3(Time.deltaTime,0,0);
+                break;
+            case 90:
+                Velocity = new Vector3(0, 0,-Time.deltaTime);
+                break;
+            case 180:
+                Velocity = new Vector3(-Time.deltaTime, 0,0);
+                break;
+            case 270:
+                Velocity = new Vector3(0,0,Time.deltaTime);
+                break;
+        }
+
+        tf.position += Velocity * speed;    
+    }       //beweegt Character naar de kijkrichting
+    void Rotate(int i)
+    {
+        tf.Rotate(0,i*90,0);
+        RotationCooldown = 1;
+    }       //Roteer de Character met 90 graden naar 1 rechts, -1 links
+    void DoRespawn()
+    {
+        agent.enabled = true;
+        respawntimer += Time.deltaTime;
+        if (respawntimer >= Respawntime)
+        {
+            dead = false;
+            agent.enabled = false;
+            tf.eulerAngles = new Vector3(0, 0, 0);
+            respawntimer = 0;
+
+        }
+        agent.SetDestination(respawn.position);
+    }         //roept NavMeshAgent aan om de character naar spawn te sturen
+    void AutoNav()
+    {
+        if (front.Collision)
+        {
+            //Ik weet niet waarom dit niet altijd goed werkt
+            //if (!right.Collision && !left.Collision)  
+            //{
+            //    int side = Random.Range(0, 2)*2-1;
+                
+            //    Debug.Log(side);
+            //    Rotate(side);
+            //}
+             if (!right.Collision)
+            {
+                Rotate(1);
+
+            }
+            else if (!left.Collision)
+            {
+                Rotate(-1);
+
+            }
+            else if (left.Collision)
+            {
+                Rotate(2);
+            }
+        }
+    }           //zorgt voor automatisch draaien tegen muren
+    void HandleInput()
+    {
+        if (Input.GetKey(Controls[0]) && RotationCooldown == 0 && !right.Collision)
+        {
+            Rotate(1); 
+        }
+        if (Input.GetKey(Controls[1]) && RotationCooldown == 0 && !left.Collision)
+        {
+            Rotate(-1);
+        }
+        if(RotationCooldown >= 1)
+        {
+            RotationCooldown += Time.deltaTime;
+            if(RotationCooldown >= 1.2f)
+            {
+                RotationCooldown = 0;
+            }
+        }
+    }       //neemt de twee inputs om te draaien, zorgt ook dat je niet een muur in kan draaien. Bevat ook draai cooldown
+}
---
+++ Assets/MultiPlayer/PlayerOnline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerOnline : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+            return;
+        CmdSpawnMyPlayer();
+	}
+
+    [SerializeField] GameObject PlayerObject;
+	// Update is called once per frame
+	void Update () {
+        return;
+	}
+
+    [Command]
+    void CmdSpawnMyPlayer()
+    {
+        GameObject go = Instantiate(PlayerObject);
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+    }
+
+}
