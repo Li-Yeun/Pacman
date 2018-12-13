27b2355bf950a870ef87888f01d3c91611a08f54
TinusPeterse <43200125+TinusPeterse@users.noreply.github.com>
2018-12-13 14:44:25 +0100

    Added Teleportation
    Je kan nu nog op kinky methodes uit de map komen maar dat wordt in de toekomst aangepast. (Het zijn geen buggs). Verder is het een beetje saaie shit met witte cubbese maar het doet nu zijn werk. Xx Tinus

---
+++ Assets/Scripts/TeleportScript2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Script dat zorgt dat de teleporters werken.
+/// </summary>
+public class TeleportScript2 : MonoBehaviour {
+    public int code = 0;
+    float dissableTimer =0;
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        if ((collider.gameObject.name == "Pacman" || collider.gameObject.name == "Blue") && dissableTimer <=0)
+        {
+            foreach (TeleportScript2 tp in FindObjectsOfType<TeleportScript2>())
+            {   
+                if (tp.code == code && tp != this)
+                {
+                    tp.dissableTimer = 2;
+                    Vector3 position = tp.gameObject.transform.position;
+                    position.y += 0.3f;
+                    collider.gameObject.transform.position = position;
+                }
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if (dissableTimer > 0)
+        {
+            dissableTimer -= Time.deltaTime;
+        }
+	}
+}
