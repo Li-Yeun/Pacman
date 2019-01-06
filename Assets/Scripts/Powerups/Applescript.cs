e312f800f607d14d6ed388da992aeb9d72840f63
TinusPeterse <43200125+TinusPeterse@users.noreply.github.com>
2019-01-06 01:16:31 +0100

    Buffixing bugfixes
    Woeps

---
+++ Assets/Scripts/Powerups/Applescript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Applescript : MonoBehaviour {
+
+    public void PacmanInstantiated()
+    {
+        AnimatorScript animatorScript = FindObjectOfType<AnimatorScript>();
+        animatorScript.Apple.Add(gameObject);
+    }
+}
