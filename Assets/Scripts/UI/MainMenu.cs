53576d934f9ad5129d33705e2ec5d470b7c5dee0
mike groffen <mikegroffen4@gmail.com>
2019-01-16 11:01:29 +0100

    removed some components from main and gamestate (option menu )

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,31 +8,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public Button loadplay, play;
-    public Button multiplayer;
-    public Button about;
     public Button exitgame;
     public string gamescene, multiplayerscene;
-    public GameObject About, buttonexplain, black;
+    public GameObject buttonexplain, black;
 
     
     public void startgame()
     { SceneManager.LoadScene(gamescene);
         black.SetActive(true); }
 
-    //loads multiplayer scenario
-    public void Multiplayer()
-    { SceneManager.LoadScene(multiplayerscene); }
-
     //enables button explanation popupscreen
     public void Buttonexplain()
     {
         buttonexplain.SetActive(true);  
-    }
-
-    //enables about menu popupscreen
-    public void AboutGame()
-    {
-        About.SetActive(true);
     }
 
     public void ExitGame()
