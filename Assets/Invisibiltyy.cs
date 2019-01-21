5db2a6ebd76e03cd2f0d0ede7a610f716c5f70e1
TinusPeterse <43200125+TinusPeterse@users.noreply.github.com>
2019-01-21 15:31:56 +0100

    GhostInvis Press Space Oranje

---
+++ Assets/Invisibiltyy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Invisibiltyy : NetworkBehaviour
+{
+    bool Invis = false;
+    GhostStates[] Ghosts;
+    Light[] lights;
+    public void Start()
+    {
+        Ghosts = gameObject.GetComponentsInChildren<GhostStates>();
+        lights = GetComponentsInChildren<Light>();
+    }
+    void Update()
+    {
+        if (!hasAuthority)
+            return;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdInvis();
+        }
+    }
+    [CommandAttribute]
+    public void CmdInvis()
+    {
+        RpcInvis();
+    }
+    [ClientRpcAttribute]
+    private void RpcInvis()
+    {
+        Invis = !Invis;
+        foreach (GhostStates Ghost in Ghosts)
+        {
+            Ghost.gameObject.SetActive(!Invis);
+        }
+        foreach (Light light in lights)
+        {
+            light.enabled = !Invis;
+        }
+    }
+    public void Reset()
+    {
+        Invis = true;
+        CmdInvis();
+    }
+}
